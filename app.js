ea42ecaf87307f3c1088dc4f8f4b57ae4c331aad
vlam7 <143142913+vlam7@users.noreply.github.com>
2023-11-28 17:17:49 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function show(){
+  document.querySelector('.menu').classList.toggle('open')
+  document.querySelector('.navigation').classList.toggle('active')
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
